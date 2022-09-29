098e7ac69524ed96362500f50db36b56c0a48910
Chenhao Liu <K21077228@kcl.ac.uk>
2022-09-29 20:31:48 +0100

    first try works

--- primes.py
+++ primes.py
@@ -1,6 +1,26 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+import math as m
+
 def primes(number_of_primes):
-    list = []
+    list = [2, 3]
+    counter = 4
+
+    if number_of_primes < 1:
+        raise ValueError
+    elif number_of_primes == 1:
+        return [2]
+    elif number_of_primes == 2:
+        return [2, 3]
+    while len(list) < number_of_primes:
+        isPrime = True
+        for i in range(2, int(m.sqrt(counter) + 1)):
+            if counter % i == 0:
+                isPrime = False
+                break
+        if isPrime:
+            list.append(counter)
+        counter += 1
     return list
